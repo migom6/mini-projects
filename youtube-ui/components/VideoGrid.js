13626a7aecb55fc47fc6bae28bb197aee08cbd16
migom6 <migom6@gmail.com>
2020-05-05 13:32:02 +0530

    youtube ui complete

---
+++ youtube-ui/components/VideoGrid.js
@@ -0,0 +1,249 @@
+import React from "react";
+
+const list = [
+  {
+    dp:
+      "https://yt3.ggpht.com/a-/AOh14GhMhCEimB3bNv_anoWxXzbQJnrtwP_otJi_Nw=s68-c-k-c0x00ffffff-no-rj-mo",
+    src:
+      "https://i.ytimg.com/vi/fFUUrcEAla8/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBNPzL14rLgBGErRnyRwrNJOnVCUQ",
+    title: "Mega Menu w/ Taildwind CSS & Vue",
+    user: "Andre Madarang",
+    views: "14k",
+    time: "10 months ago",
+  },
+  {
+    dp:
+      "https://yt3.ggpht.com/a-/AOh14GgwH01tAWeg501SpPFxdz88PwyI8HtMLizB4Q=s68-c-k-c0x00ffffff-no-rj-mo",
+    src:
+      "https://i.ytimg.com/vi/vIKD27qF78o/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLA8umJWSowVOx0s58EIXm5Df3cmHg",
+    title: "Mega Menu w/ Taildwind CSS & Vue",
+    user: "Andre Madarang",
+    views: "14k",
+    time: "10 months ago",
+  },
+  {
+    dp:
+      "https://yt3.ggpht.com/a-/AOh14GjeuP4PiKXofO8gQ6VuxBJMJX1oW8uA-aP8OQ=s68-c-k-c0x00ffffff-no-rj-mo",
+    src:
+      "https://i.ytimg.com/vi/YN0nHY3GNec/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBoOHkE5_5SKCd8fGaTs4u7Pr8p2Q",
+    title: "Mega Menu w/ Taildwind CSS & Vue",
+    user: "Andre Madarang",
+    views: "14k",
+    time: "10 months ago",
+  },
+  {
+    dp:
+      "https://yt3.ggpht.com/a-/AOh14GhE9Z9t5I-cr77Lix84O6aLsgGz7v-9tA4daA=s68-c-k-c0x00ffffff-no-rj-mo",
+    src:
+      "https://i.ytimg.com/vi/aeWxoH3P17A/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAdt0QYpq6LIfFr9TmEPl8-Tdm2Ew",
+    title: "Mega Menu w/ Taildwind CSS & Vue",
+    user: "Andre Madarang",
+    views: "14k",
+    time: "10 months ago",
+  },
+  {
+    dp:
+      "https://yt3.ggpht.com/a-/AOh14GiKpFE36WnwhWjFI1uDRelUHQUi9RL5XlUOgQ=s68-c-k-c0x00ffffff-no-rj-mo",
+    src:
+      "https://i.ytimg.com/vi/Suxm7LUTunU/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCkqBlG_scQ_U3su5HRNyMX7xQOeg",
+    title: "Mega Menu w/ Taildwind CSS & Vue",
+    user: "Andre Madarang",
+    views: "14k",
+    time: "10 months ago",
+  },
+  {
+    dp:
+      "https://yt3.ggpht.com/a-/AOh14GiHvIK4knTrSNny3m1tIJUhvSoP8-CoyTnlAw=s68-c-k-c0x00ffffff-no-rj-mo",
+    src:
+      "https://i.ytimg.com/vi/pdj5IlL4qPI/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCKXoTP_9pCVOuUOyGAVW8X9UL1Xw",
+    title: "Mega Menu w/ Taildwind CSS & Vue",
+    user: "Andre Madarang",
+    views: "14k",
+    time: "10 months ago",
+  },
+  {
+    dp:
+      "https://yt3.ggpht.com/a-/AOh14Gh02ndFKNAdl-0TMsvjQ-wEnLQt8PgvU7zIKg=s68-c-k-c0x00ffffff-no-rj-mo",
+    src:
+      "https://i.ytimg.com/vi/qz9tKlF431k/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAVbblDOubG4cg41j65S_3Qs0xRsw",
+    title: "Mega Menu w/ Taildwind CSS & Vue",
+    user: "Andre Madarang",
+    views: "14k",
+    time: "10 months ago",
+  },
+  {
+    dp:
+      "https://yt3.ggpht.com/a-/AOh14GhYkw9AdqshBkYBeJTGVg5DIj7nZ12xE0IPoA=s68-c-k-c0x00ffffff-no-rj-mo",
+    src:
+      "https://i.ytimg.com/vi/2aljiS8CqtU/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLD4kXK0Hk8XmeZD8qN3gyp09dqoeg",
+    title: "Mega Menu w/ Taildwind CSS & Vue",
+    user: "Andre Madarang",
+    views: "14k",
+    time: "12 months ago",
+  },
+  {
+    dp:
+      "https://yt3.ggpht.com/a-/AOh14GgwH01tAWeg501SpPFxdz88PwyI8HtMLizB4Q=s68-c-k-c0x00ffffff-no-rj-mo",
+    src:
+      "https://i.ytimg.com/vi/vIKD27qF78o/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLA8umJWSowVOx0s58EIXm5Df3cmHg",
+    title: "Mega Menu w/ Taildwind CSS & Vue",
+    user: "Andre Madarang",
+    views: "14k",
+    time: "10 months ago",
+  },
+  {
+    dp:
+      "https://yt3.ggpht.com/a-/AOh14GjeuP4PiKXofO8gQ6VuxBJMJX1oW8uA-aP8OQ=s68-c-k-c0x00ffffff-no-rj-mo",
+    src:
+      "https://i.ytimg.com/vi/YN0nHY3GNec/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBoOHkE5_5SKCd8fGaTs4u7Pr8p2Q",
+    title: "Mega Menu w/ Taildwind CSS & Vue",
+    user: "Andre Madarang",
+    views: "14k",
+    time: "10 months ago",
+  },
+  {
+    dp:
+      "https://yt3.ggpht.com/a-/AOh14GhE9Z9t5I-cr77Lix84O6aLsgGz7v-9tA4daA=s68-c-k-c0x00ffffff-no-rj-mo",
+    src:
+      "https://i.ytimg.com/vi/aeWxoH3P17A/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAdt0QYpq6LIfFr9TmEPl8-Tdm2Ew",
+    title: "Mega Menu w/ Taildwind CSS & Vue",
+    user: "Andre Madarang",
+    views: "14k",
+    time: "10 months ago",
+  },
+  {
+    dp:
+      "https://yt3.ggpht.com/a-/AOh14GiKpFE36WnwhWjFI1uDRelUHQUi9RL5XlUOgQ=s68-c-k-c0x00ffffff-no-rj-mo",
+    src:
+      "https://i.ytimg.com/vi/Suxm7LUTunU/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCkqBlG_scQ_U3su5HRNyMX7xQOeg",
+    title: "Mega Menu w/ Taildwind CSS & Vue",
+    user: "Andre Madarang",
+    views: "14k",
+    time: "10 months ago",
+  },
+  {
+    dp:
+      "https://yt3.ggpht.com/a-/AOh14GiHvIK4knTrSNny3m1tIJUhvSoP8-CoyTnlAw=s68-c-k-c0x00ffffff-no-rj-mo",
+    src:
+      "https://i.ytimg.com/vi/pdj5IlL4qPI/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCKXoTP_9pCVOuUOyGAVW8X9UL1Xw",
+    title: "Mega Menu w/ Taildwind CSS & Vue",
+    user: "Andre Madarang",
+    views: "14k",
+    time: "10 months ago",
+  },
+  {
+    dp:
+      "https://yt3.ggpht.com/a-/AOh14GhMhCEimB3bNv_anoWxXzbQJnrtwP_otJi_Nw=s68-c-k-c0x00ffffff-no-rj-mo",
+    src:
+      "https://i.ytimg.com/vi/HdxvF_dOfO0/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDcghX-cfN9pwt1DRaHGsJvwpcSCA",
+    title: "Mega Menu w/ Taildwind CSS & Vue",
+    user: "Andre Madarang",
+    views: "14k",
+    time: "10 months ago",
+  },
+  {
+    dp:
+      "https://yt3.ggpht.com/a-/AOh14GhMhCEimB3bNv_anoWxXzbQJnrtwP_otJi_Nw=s68-c-k-c0x00ffffff-no-rj-mo",
+    src:
+      "https://i.ytimg.com/vi/HdxvF_dOfO0/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDcghX-cfN9pwt1DRaHGsJvwpcSCA",
+    title: "Mega Menu w/ Taildwind CSS & Vue",
+    user: "Andre Madarang",
+    views: "14k",
+    time: "10 months ago",
+  },
+  {
+    dp:
+      "https://yt3.ggpht.com/a-/AOh14GhMhCEimB3bNv_anoWxXzbQJnrtwP_otJi_Nw=s68-c-k-c0x00ffffff-no-rj-mo",
+    src:
+      "https://i.ytimg.com/vi/HdxvF_dOfO0/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDcghX-cfN9pwt1DRaHGsJvwpcSCA",
+    title: "Mega Menu w/ Taildwind CSS & Vue",
+    user: "Andre Madarang",
+    views: "14k",
+    time: "10 months ago",
+  },
+  {
+    dp:
+      "https://yt3.ggpht.com/a-/AOh14GhMhCEimB3bNv_anoWxXzbQJnrtwP_otJi_Nw=s68-c-k-c0x00ffffff-no-rj-mo",
+    src:
+      "https://i.ytimg.com/vi/HdxvF_dOfO0/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDcghX-cfN9pwt1DRaHGsJvwpcSCA",
+    title: "Mega Menu w/ Taildwind CSS & Vue",
+    user: "Andre Madarang",
+    views: "14k",
+    time: "10 months ago",
+  },
+  {
+    dp:
+      "https://yt3.ggpht.com/a-/AOh14GhMhCEimB3bNv_anoWxXzbQJnrtwP_otJi_Nw=s68-c-k-c0x00ffffff-no-rj-mo",
+    src:
+      "https://i.ytimg.com/vi/HdxvF_dOfO0/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDcghX-cfN9pwt1DRaHGsJvwpcSCA",
+    title: "Mega Menu w/ Taildwind CSS & Vue",
+    user: "Andre Madarang",
+    views: "14k",
+    time: "10 months ago",
+  },
+  {
+    dp:
+      "https://yt3.ggpht.com/a-/AOh14GhMhCEimB3bNv_anoWxXzbQJnrtwP_otJi_Nw=s68-c-k-c0x00ffffff-no-rj-mo",
+    src:
+      "https://i.ytimg.com/vi/HdxvF_dOfO0/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDcghX-cfN9pwt1DRaHGsJvwpcSCA",
+    title: "Mega Menu w/ Taildwind CSS & Vue",
+    user: "Andre Madarang",
+    views: "14k",
+    time: "10 months ago",
+  },
+  {
+    dp:
+      "https://yt3.ggpht.com/a-/AOh14GhMhCEimB3bNv_anoWxXzbQJnrtwP_otJi_Nw=s68-c-k-c0x00ffffff-no-rj-mo",
+    src:
+      "https://i.ytimg.com/vi/HdxvF_dOfO0/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDcghX-cfN9pwt1DRaHGsJvwpcSCA",
+    title: "Mega Menu w/ Taildwind CSS & Vue",
+    user: "Andre Madarang",
+    views: "14k",
+    time: "10 months ago",
+  },
+  {
+    dp:
+      "https://yt3.ggpht.com/a-/AOh14GhMhCEimB3bNv_anoWxXzbQJnrtwP_otJi_Nw=s68-c-k-c0x00ffffff-no-rj-mo",
+    src:
+      "https://i.ytimg.com/vi/HdxvF_dOfO0/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDcghX-cfN9pwt1DRaHGsJvwpcSCA",
+    title: "Mega Menu w/ Taildwind CSS & Vue",
+    user: "Andre Madarang",
+    views: "14k",
+    time: "10 months ago",
+  },
+];
+
+function VideoCard({ info }) {
+  const { src, title, user, views, time, dp } = info;
+  return (
+    <div className="group flex flex-col cursor-pointer ">
+      <div className="relative ">
+        <img className="" src={src} alt="video" />
+        <div className="hidden group-hover:flex absolute inset-0 justify-center items-center">
+          <i className="fas fa-play text-3xl text-opacity-50"></i>
+        </div>
+      </div>
+      <div className="flex mt-4">
+        <div className="mr-2 mt-1">
+          <img className="w-6 h-6 rounded-full" src={dp} alt="dp" />
+        </div>
+        <div className="flex flex-col  text-sm">
+          <span className="text-white font-semibold">{title}</span>
+          <span className="text-yt-text">{user}</span>
+          <span className="text-yt-text">{`${views} views . ${time}`}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function VideoGrid() {
+  return (
+    <div className="mt-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 col-gap-5 row-gap-12">
+      {list.map((item) => {
+        return <VideoCard info={item} />;
+      })}
+    </div>
+  );
+}
+export default VideoGrid;
--- youtube-ui/pages/index.js
+++ youtube-ui/pages/index.js
@@ -1,4 +1,5 @@
 import SidebarSection from "../components/SidebarSection";
+import VideoGrid from "../components/VideoGrid";
 
 const list1 = [
   { icon: "home", title: "Home" },
@@ -98,11 +99,11 @@ export default function Home() {
         {/* logo */}
         <div className="w-56  flex items-center  ">
           {/* hamburger */}
-          <div className="ml-6 text-lg text-white">
+          <div className="ml-6 text-lg text-white cursor-pointer">
             <i className="fas fa-bars "></i>
           </div>
           {/* y-t icon */}
-          <div className="relative ml-6 pr-1">
+          <div className="relative ml-6 pr-1 cursor-pointer">
             <div
               className="absolute bg-white w-4 h-3 z-0"
               style={{ top: "0.5em", right: "0.4em" }}
@@ -113,7 +114,7 @@ export default function Home() {
             ></i>
           </div>
           {/* y-t text */}
-          <div className="text-white tracking-tightest text-xl font-semibold ">
+          <div className="text-white tracking-tightest text-xl font-semibold cursor-pointer ">
             YouTube
           </div>
         </div>
@@ -124,23 +125,23 @@ export default function Home() {
               className=" w-full h-full max-w-lg rounded-sm px-3 bg-yt-input text-yt-text"
               placeholder="Search"
             />
-            <button className="w-16 h-full rounded-sm text-yt-text bg-yt-lightest focus:outline-none">
+            <button className="w-16 h-full rounded-sm text-yt-text bg-yt-lightest focus:outline-none cursor-pointer">
               <i className=" fas fa-search"></i>
             </button>
           </div>
         </div>
         {/* yt action buttons */}
         <div className="w-64 flex justify-end">
-          <div className="mr-8 text-lg text-white">
+          <div className="mr-8 text-lg text-white cursor-pointer">
             <i className="fas fa-video"></i>
           </div>
-          <div className="mr-8 text-lg text-white">
+          <div className="mr-8 text-lg text-white cursor-pointer">
             <i className="fas fa-border-none "></i>
           </div>
-          <div className="mr-8 text-lg text-white">
+          <div className="mr-8 text-lg text-white cursor-pointer">
             <i className="fas fa-bell"></i>
           </div>
-          <div className="mr-8 ">
+          <div className="mr-8 cursor-pointer">
             <img
               className="w-8 h-8 rounded-full"
               src="https://lh3.googleusercontent.com/a-/AOh14GheSWRwqeTZyhIXwO6YEDuGAd4VzscgJDyQh6ES6g=s88-c-k-c0x00ffffff-no-rj-mo"
@@ -157,8 +158,9 @@ export default function Home() {
           <SidebarSection data={list2} />
           <SidebarSection data={list3} heading="SUBSCRIPTIONS" />
         </div>
-        <div className="flex justify-center flex-1 bg-yt-feed text-5xl text-yt-text">
-          main
+        <div className="flex flex-col flex-1 bg-yt-feed text-white px-6 py-8 overflow-y-auto">
+          <h1 className=" font-bold text-2xl">Recommended</h1>
+          <VideoGrid />
         </div>
       </div>
     </div>
